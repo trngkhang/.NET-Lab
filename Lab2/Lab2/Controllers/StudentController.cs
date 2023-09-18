3d95710ef115b5f158600b191ba92f1781067156
Khang <nguyenkhangdev@gmail.com>
2023-09-18 21:27:00 +0700

    add Lab2 2.1

---
+++ Lab2/Lab2/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+	public class StudentController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Details()
+		{
+			return View("Details");
+		}
+		public ActionResult Manage(StudentInfo model, String command)
+		{
+			var path = Path.Combine(Directory.GetCurrentDirectory(),
+			"wwwroot", "Student.txt");
+			if (command == "Lưu")
+			{
+				String[] lines = { model.Id, model.Name, model.Marks.ToString() };
+				System.IO.File.WriteAllLines(path, lines);
+				ViewBag.Message = "Đã ghi vào file !";
+			}
+			else if (command == "Mở")
+			{
+				String[] lines = System.IO.File.ReadAllLines(path);
+				ViewBag.Id = lines[0];
+				ViewBag.Name = lines[1];
+				ViewBag.Marks = Convert.ToDouble(lines[2]);
+				ViewBag.Message = "Đã đọc từ file !";
+			}
+			return View("Details");
+		}
+	}
+}
---
+++ Lab2/Lab2/Models/StudentInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab2.Models
+{
+	public class StudentInfo
+	{
+		public string? Id { get; set; }
+		public string? Name { get; set; }
+		public double Marks { get; set; }
+	}
+}
